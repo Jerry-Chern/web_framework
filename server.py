af890f261eae2e690ef9768348e3df462e7eef88
Jel <309216506@qq.com>
2019-08-16 10:43:11 +0800

    update server.py

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 import socket
 import _thread
 
+
 from models.base_model import SQLModel
 from request import Request
 from utils import log
 
+
 from routes import error
+
 
 from routes.routes_weibo import route_dict as weibo_routes
 from routes.routes_user import route_dict as user_routes
@@ -19,11 +22,12 @@ def response_for_path(request):
     """
     r = {}
     # 注册外部的路由
+    # r.update(todo_routes())
     r.update(weibo_routes())
     r.update(user_routes())
     r.update(public_routes())
     response = r.get(request.path, error)
-    log('路由分发 <{}> -> <{}>'.format(request.path, response))
+    log('request path <{}> ------> route <{}>'.format(request.path, response.__name__))
     return response(request)
 
 
@@ -36,18 +40,19 @@ def request_from_connection(connection):
         # 取到的数据长度不够 buffer_size 的时候，说明数据已经取完了。
         if len(r) < buffer_size:
             request = request.decode()
-            log('HTTP 请求: <{}>'.format(request.encode()))
+            # log('request\n {}'.format(request))
             return request
 
 
 def process_request(connection):
     with connection:
         r = request_from_connection(connection)
-        # 把原始登录用户请求数据传给 Request 对象
+        # log('request log:\n <{}>'.format(r))
+        # 把原始请求数据传给 Request 对象
         request = Request(r)
         # 用 response_for_path 函数来得到 path 对应的响应内容
         response = response_for_path(request)
-        log("HTTP 响应:\n <{}>".format(response))
+        # log("response log:\n <{}>".format(response))
         # 把响应发送给客户端
         connection.sendall(response)
 
@@ -56,12 +61,9 @@ def run(host, port):
     """
     启动服务器
     """
-    # 初始化数据库
+    # 初始化 ORM
     SQLModel.init_db()
-    log('数据库链接初始化 <{}>'.format(SQLModel.connection.host_info))
-    # 初始化 socket 套路
-    # 使用 with 可以保证程序中断的时候正确关闭 socket 释放占用的端口
-    log('开始运行于 http://{}:{}'.format(host, port))
+    log('开始运行于', 'http://{}:{}'.format(host, port))
     with socket.socket() as s:
         s.bind((host, port))
         # 监听 接受 读取请求数据 解码成字符串
@@ -70,7 +72,7 @@ def run(host, port):
         while True:
             connection, address = s.accept()
             # 第二个参数类型必须是 tuple
-            log('请求地址 {}'.format(address))
+            # log('ip {}'.format(address))
             _thread.start_new_thread(process_request, (connection,))
 
 
@@ -78,6 +80,6 @@ if __name__ == '__main__':
     # 生成配置并且运行程序
     config = dict(
         host='127.0.0.1',
-        port=3800,
+        port=3600,
     )
     run(**config)
